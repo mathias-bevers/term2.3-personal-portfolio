bd923cc41c0e43141943f9723b6dd636cba190ae
Mathias Bevers <mathiasbevers@outlook.com>
2024-03-01 16:39:32 +0100

    added the register form and moved login to partial view.
    - moved the login to be a partial view.
    - added a register form which is another partial view.
    - views are switched using session keys.
    - switched forms the be own "tables" which gave more layout control.
    - next add admin rights to the admin user and shield create pages.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,6 +6,12 @@ namespace PersonalPortfolio.Controllers
 {
     public class UserController : Controller
     {
+        public enum AuthorizationState
+        {
+            Login,
+            Register
+        };
+
         public IActionResult Index()
         {
             return View();
@@ -14,11 +20,11 @@ namespace PersonalPortfolio.Controllers
         [HttpPost]
         public async Task<IActionResult> Authorize(User user)
         {
-            await using var context = new LibraryDbContext();
+            await using var db = new LibraryDbContext();
 
             if (!ModelState.IsValid) { return View("Index", user); }
 
-            User? userDetail = await context.Users.FirstOrDefaultAsync(
+            User? userDetail = await db.Users.FirstOrDefaultAsync(
                 x => x.UserName == user.UserName && x.Password == user.Password);
 
             if (ReferenceEquals(userDetail, null))
@@ -26,8 +32,33 @@ namespace PersonalPortfolio.Controllers
                 user.ErrorMessage = "Invalid credentials!";
                 return View("Index", user);
             }
-            
+
             HttpContext.Session.SetSessionObjectAsJson(Settings.SESSION_USER_KEY, userDetail);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user, string confirmPW)
+        {
+            await using var db = new LibraryDbContext();
+            
+            if (!ModelState.IsValid) { return View("Index", user); }
+            
+            if (!confirmPW.Equals(user.Password))
+            {
+                ModelState.AddModelError("Password", "The passwords are not the same");
+                return View("Index", user);
+            }
+
+            if (await db.Users.AnyAsync(u => u.UserName.Equals(user.UserName)))
+            {
+                ModelState.AddModelError("UserName", $"The username \'{user.UserName}\' is already taken");
+                return View("Index", user);
+            }
+
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            HttpContext.Session.SetSessionObjectAsJson(Settings.SESSION_USER_KEY, user);
             return RedirectToAction("Index");
         }
 
@@ -35,6 +66,12 @@ namespace PersonalPortfolio.Controllers
         {
             HttpContext.Session.Clear();
             return RedirectToAction("Index");
+        }
+
+        public ActionResult SwitchPartialPage(AuthorizationState state)
+        {
+            HttpContext.Session.SetSessionObjectAsJson(Settings.LOGIN_PAGE_ID_KEY, (int)state);
+            return View("Index");
         }
     }
 }
--- src/Settings.cs
+++ src/Settings.cs
@@ -3,5 +3,6 @@ namespace PersonalPortfolio
     public static class Settings
     {
         public const string SESSION_USER_KEY = "_user";
+        public const string LOGIN_PAGE_ID_KEY = "_login_page_id";
     }
 }
